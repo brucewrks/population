256fd1439b29c53c9052810f3771fee7248ac11b
Bruce Caldwell <bruce@myinbox.ws>
2019-03-23 16:41:07 -0400

    Ready to push to GitHub

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # The Project
+
+I'm not entirely sure what this is. I just wanted to explore the idea of controlling population given limited resources...
+
+Run with `npm`.
--- program.js
+++ program.js
@@ -4,4 +4,4 @@ const World = require('./lib/World');
 
 let world = new World();
 server.addDataPoint('world', world);
-server.listen(1337, () => console.log('Server listening on port 1337'));
+server.listen(1337, () => console.log('Server listening at http://localhost:1337'));
